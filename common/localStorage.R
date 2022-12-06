0c8825154501b6fcf54d156ef29ac2a277d5be50
andrei pazniak <andrei1pazniak@gmail.com>
2022-12-06 13:13:46 +0200

    different type of correlation XBRUSD DXYUSD

--- common/localStorage.R
+++ common/localStorage.R
@@ -9,3 +9,17 @@ localStorage_getSeries <- function(series_id, startDate=ISOdate(1990, 1, 1, tz =
   d[, series_id:=series_id]
   return(d)
 }
+
+#tempconvertor to Date,"Closing Value"
+TempConvertor <- function(){
+  require(data.table)
+  require(lubridate)
+  d <- fread(paste0("..\\data\\raw\\investing\\", "US Dollar Index Historical Data.csv"))
+  d2 <- fread(paste0("..\\data\\raw\\investing\\", "US Dollar Index Historical Data (1).csv"))
+  d3 <- fread(paste0("..\\data\\raw\\investing\\", "US Dollar Index Historical Data (2).csv"))
+  d <- rbind(d, d2, d3)
+  d <- unique(d, by="Date")
+  d[,Date:=parse_date_time(Date, 'mdy')]
+  d<- d[order(Date)]
+  fwrite(d[,.(Date=as.Date(Date), `"Closing Value"`=Price)], paste0("..\\data\\raw\\investing\\", "DXY"))
+}
