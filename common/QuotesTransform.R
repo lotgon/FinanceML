0fd82dbbaa3fa3ed4a57d6702e45b3cbe6104039
andrei pazniak <andrei1pazniak@gmail.com>
2024-01-02 02:04:27 +0200

    When to buy SPX

--- common/QuotesTransform.R
+++ common/QuotesTransform.R
@@ -1,25 +1,25 @@
 #series: data.table with value column
-TranformToDropdawnData<-function(series){
+TranformToDropdawnData<-function(series, columnName="value"){
 #  series_ids <- series[,unique(series_id)]
-  suppressWarnings({series[,maxValue:=purrr::accumulate(value, max, na.rm=T), by=series_id]})
-  series[,dropdawn:=value/maxValue, by=series_id]
+  suppressWarnings({series[,maxValue:=purrr::accumulate(get(columnName), max, na.rm=T), by=series_id]})
+  series[,dropdawn:=get(columnName)/maxValue, by=series_id]
   series[,dropdawnMVA:=frollmean(dropdawn, 7), by=series_id]
   series[dropdawnMVA==-Inf, dropdawnMVA:=NA]
   series
 }
-CalculateLastMax <- function(series, threshold=0.9){
+TranformTo_LastMax <- function(series, threshold=0.9, columnName="value"){
   #in parameters
   #quotes as data.table[series_id, value] ordered by time
   for(sym in series[,unique(series_id)])
-    CalculateLastMaxPerSeriesId(series, sym, threshold)
+    TranformTo_LastMaxPerSeriesId(series, sym, threshold, columnName)
   series
 }
-CalculateLastMaxPerSeriesId <- function(quotes, chosen_series_id, threshold){
+TranformTo_LastMaxPerSeriesId <- function(quotes, chosen_series_id, threshold, columnName="value"){
   #in parameters
   #quotes as data.table[series_id, value] ordered by time
   cPrevMax <- numeric(quotes[series_id==chosen_series_id, .N])
   i <- 0;prevMax_ <- NA;currentMax <- 0;isTrendUp <- TRUE
-  for(price in quotes[series_id==chosen_series_id,value]){
+  for(price in quotes[series_id==chosen_series_id,get(columnName)]){
     i<-i+1
     if(!is.na(price)){      
       if( price > currentMax){
